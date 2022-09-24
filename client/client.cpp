1678f508b5d2d1461937737aa2a3721d22ee5772
elizanikol <nikolveta0713@gmail.com>
2022-09-24 11:46:52 +0300

    init

---
+++ client/client.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <cmath>
+#include <filesystem>
+
+
+using Matrix = std::vector<std::vector<double>>;
+const std::string InputDataFile{ "../../Test.b" };
+const std::string ResultsFile{ "../../Results.b" };
+const size_t MBMultiplier = 1024 * 1024;
+
+
+void ReadMatricesFromFile(const std::string& path, std::vector<Matrix>& matrices)
+{
+    std::ifstream file(path, std::ifstream::binary);
+
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file: " << path << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    size_t rowsNumber = 0;
+    while (file.read(reinterpret_cast<char*>(&rowsNumber), sizeof(rowsNumber)))
+    {
+        Matrix matrix(rowsNumber, std::vector<double>(rowsNumber));
+        for (auto& row : matrix)
+        {
+            if (!file.read(reinterpret_cast<char*>(&row[0]), static_cast<std::streamsize>(rowsNumber) * sizeof(double)))
+            {
+                std::cerr << "Reading from file: " << path << " failed\n";
+                std::exit(EXIT_FAILURE);
+            }
+        }
+        matrices.push_back(matrix);
+    }
+
+    file.close();
+}
+
+template<class ForwardIt>
+ForwardIt MaxElement(ForwardIt itFirst, ForwardIt itLast)
+{
+    if (itFirst == itLast)
+    {
+        return itLast;
+    }
+
+    auto itMax = itFirst;
+    ++itFirst;
+    for (; itFirst != itLast; ++itFirst)
+    {
+        if (*itMax < *itFirst)
+        {
+            itMax = itFirst;
+        }
+    }
+    return itMax;
+}
+
+void CalculateMaxElementsSquare(const std::vector<Matrix>& matrices, std::vector<std::vector<double>>& maxSquaresVectors)
+{
+    for (size_t i = 0; i < matrices.size(); ++i)
+    {
+        maxSquaresVectors[i].resize(matrices[i].size());
+        for (size_t row = 0; row < matrices[i].size(); ++row)
+        {
+            const auto maxElementIt = MaxElement(std::begin(matrices[i][row]), std::end(matrices[i][row]));
+            if (maxElementIt == std::end(matrices[i][row]))
+            {
+                std::cerr << "Empty row in square matrix\n";
+                std::exit(EXIT_FAILURE);
+            }
+            maxSquaresVectors[i][row] = pow(*maxElementIt, 2.0);
+        }
+    }
+}
+
+void SaveResults(const std::string& path, const std::vector<std::vector<double>>& vectors, const std::chrono::duration<double> elapsedSeconds)
+{
+    std::ofstream file(path, std::ofstream::binary);
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file: " << path << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    for (auto& v : vectors)
+    {
+        file.write(reinterpret_cast<const char*>(&v[0]), static_cast<std::streamsize>(v.size()) * sizeof(double));
+    }
+
+    const auto fileSizeInBytes = std::filesystem::file_size(InputDataFile);
+    file << "\nElapsed time: " << elapsedSeconds.count() << "s for data of size: " << fileSizeInBytes 
+        << " bytes (" << fileSizeInBytes / MBMultiplier << ") MB.";
+
+    file.close();
+    if (!file)
+    {
+        std::cerr << "Writing to file: " << path << " failed\n";
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+int main()
+{
+    std::vector<Matrix> matrices;
+    ReadMatricesFromFile(InputDataFile, matrices);
+
+    std::vector<std::vector<double>> maxSquaresVectors(matrices.size());
+    auto start = std::chrono::steady_clock::now();
+    CalculateMaxElementsSquare(matrices, maxSquaresVectors);
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsedSeconds = end - start;
+
+    SaveResults(ResultsFile, maxSquaresVectors, elapsedSeconds);
+
+    const auto fileSizeInBytes = std::filesystem::file_size(InputDataFile);
+    std::cout << "elapsed time: " << elapsedSeconds.count() << "s for data of size: "
+        << fileSizeInBytes << " bytes (" << fileSizeInBytes / MBMultiplier << ") MB.\n";
+}
---
+++ generator/generator.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <fstream>
+#include <random>
+
+
+const std::string InputDataFile{"../../Test.b"};
+const size_t MBMultiplier = 1024 * 1024;
+const size_t MaxRowsNumber = 50;
+const size_t MinRowsNumber = 2;
+const size_t MaxElementValue = 10;
+const size_t MinElementValue = 0;
+
+
+void GenerateMatricesAndSaveThemToFile(const std::string& path, const size_t fileSizeInMB)
+{
+    std::default_random_engine gen(std::random_device{}());
+    std::uniform_int_distribution<size_t> distSize(MinRowsNumber, MaxRowsNumber);
+    std::uniform_real_distribution<double> distElement(MinElementValue, MaxElementValue);
+
+    std::ofstream file(path, std::ofstream::binary);
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file: " << path << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    size_t totalBytesWritten = 0;
+    const auto fileSizeInBytes = fileSizeInMB * MBMultiplier;
+    while (totalBytesWritten < fileSizeInBytes)
+    {
+        const size_t rowsNumber = distSize(gen);
+        file.write(reinterpret_cast<const char*>(&rowsNumber), sizeof(rowsNumber));
+
+        std::vector<double> matrix(static_cast<size_t>(std::pow(rowsNumber, 2)));
+        std::generate(matrix.begin(), matrix.end(), [&gen, &distElement]() mutable { return distElement(gen); });
+        file.write(reinterpret_cast<const char*>(&matrix[0]), static_cast<std::streamsize>(matrix.size()) * sizeof(double));
+        totalBytesWritten += sizeof(rowsNumber) + matrix.size() * sizeof(double);
+    }
+
+    file.close();
+    if (!file) 
+    {
+        std::cerr << "Writing to file: " << path << " failed\n";
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+
+int main()
+{
+    std::cout << "Enter file size in MB" << std::endl;
+    size_t fileSizeInMB = 0;
+    std::cin >> fileSizeInMB;
+
+    GenerateMatricesAndSaveThemToFile(InputDataFile, fileSizeInMB);
+    std::cout << "Data successfully generated into: " << InputDataFile;
+}
